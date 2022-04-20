0971e86203d7a422a442d8ed928c63abb57b5ec6
saisilinus <saisilinus19@gmail.com>
2022-04-20 11:02:29 +0300

    feat: add developer helpers
    add developer helpers such as prettier, eslint, commitlint, husky, and commitizen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
