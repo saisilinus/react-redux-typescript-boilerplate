29c895d6759cd9b2692741a271332e17a633bd7a
saisilinus <saisilinus19@gmail.com>
2022-05-01 15:02:09 +0300

    feat: add register page
    add page and functionality for registering a user

--- src/modules/auth/Login.tsx
+++ src/modules/auth/Login.tsx
@@ -3,7 +3,6 @@ import { Container, Card, Row, Col, Form, InputGroup, FormCheck, Button } from '
 import { useLocation, useNavigate, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faEnvelope, faUnlockAlt } from '@fortawesome/free-solid-svg-icons';
-import BgImage from '../../assets/img/illustrations/signin.svg';
 import { routes } from '../routing';
 import { useLoginMutation } from './auth.api';
 import Loader from '../common/loader/Loader';
@@ -61,13 +60,13 @@ const Login = () => {
       <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
         <Container>
           <p className="text-center">
-            <Card.Link as={Link} to={routes.Home.path} className="text-gray-700">
+            <Card.Link as={Link} to={routes.Home.path} className="text-dark">
               <FontAwesomeIcon icon={faAngleLeft} className="me-2" /> Back to homepage
             </Card.Link>
           </p>
-          <Row className="justify-content-center form-bg-image" style={{ backgroundImage: `url(${BgImage})` }}>
+          <Row className="justify-content-center">
             <Col xs={12} className="d-flex align-items-center justify-content-center">
-              <div className="bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+              <div className="bg-white border rounded border-light p-4 p-lg-5 w-lg-50 w-md-100">
                 <div className="text-center text-md-center mb-4 mt-md-0">
                   <h3 className="mb-0">Sign in to our platform</h3>
                 </div>
@@ -131,7 +130,7 @@ const Login = () => {
                 <div className="d-flex justify-content-center align-items-center mt-4">
                   <span className="fw-normal">
                     Not registered?
-                    <Card.Link as={Link} to={routes.Register.path} className="fw-bold">
+                    <Card.Link as={Link} to={`/${routes.Register.path}`} className="fw-bold">
                       {` Create account `}
                     </Card.Link>
                   </span>
--- src/modules/auth/Register.tsx
+++ src/modules/auth/Register.tsx
@@ -1,7 +1,113 @@
-import React from 'react';
+import { faEnvelope, faUnlockAlt, faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import Loader from '../common/loader/Loader';
+import { useRegisterMutation } from './auth.api';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [registerUser, { isLoading }] = useRegisterMutation();
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const rememberMe = localStorage.getItem('rememberMe');
+
+    if (name === '' || email === '' || password === '') {
+      toast('Please fill in the values first');
+    }
+
+    await registerUser({ name, email, password })
+      .unwrap()
+      .then((payload) => {
+        if (rememberMe === 'true') {
+          localStorage.setItem('accessToken', payload.tokens.access.token);
+          localStorage.setItem('refreshToken', payload.tokens.refresh.token);
+          localStorage.setItem('userId', payload.user.id);
+        } else {
+          sessionStorage.setItem('accessToken', payload.tokens.access.token);
+          sessionStorage.setItem('refreshToken', payload.tokens.refresh.token);
+          sessionStorage.setItem('userId', payload.user.id);
+        }
+        navigate('/');
+      });
+  }
+
+  return (
+    <main>
+      <Loader show={isLoading} />
+      <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+        <Container>
+          <Row className="justify-content-center">
+            <Col xs={12} className="d-flex align-items-center justify-content-center">
+              <div className="bg-white border rounded border-light p-4 p-lg-5 w-lg-50 w-md-100">
+                <div className="text-center text-md-center mb-4 mt-md-0">
+                  <h3 className="mb-0">Register</h3>
+                </div>
+                <Form className="mt-4" onSubmit={handleSubmit}>
+                  <Form.Group id="name" className="mb-4">
+                    <Form.Label>Your Name</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faUser} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        name="name"
+                        autoFocus
+                        required
+                        type="text"
+                        placeholder="John Doe"
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group id="email" className="mb-4">
+                    <Form.Label>Your Email</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faEnvelope} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        name="email"
+                        autoFocus
+                        required
+                        type="email"
+                        placeholder="example@company.com"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group id="password" className="mb-4">
+                    <Form.Label>Your Password</Form.Label>
+                    <InputGroup>
+                      <InputGroup.Text>
+                        <FontAwesomeIcon icon={faUnlockAlt} />
+                      </InputGroup.Text>
+                      <Form.Control
+                        name="password"
+                        required
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                  <Button variant="primary" type="submit" className="w-100">
+                    Register
+                  </Button>
+                </Form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </main>
+  );
 };
 
 export default Register;
