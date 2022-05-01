54aad483f26b611be1619e5540a992c65b213b8b
saisilinus <saisilinus19@gmail.com>
2022-05-01 10:24:45 +0300

    feat: add page routing animation
    add animation for switching from one page to another

--- src/modules/routing/Routing.tsx
+++ src/modules/routing/Routing.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import RequireAuth from './RequireAuth';
 import { Lock, Login, NotFound, Register, ResetPassword, VerifyEmail } from '../auth';
 import { DashboardHome, Profile } from '../dashboard';
@@ -9,25 +9,43 @@ import routes from './routes';
 import Layout from '../common/layout/Layout';
 
 const Routing = () => {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState('fadeIn');
+
+  useEffect(() => {
+    if (location !== displayLocation) setTransistionStage('fadeOut');
+  }, [location, displayLocation]);
+
   return (
-    <Routes>
-      <Route path={routes.Home.path} element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path={routes.Login.path} element={<Login />} />
-        <Route path={routes.Register.path} element={<Register />} />
-        <Route path={routes.Lock.path} element={<Lock />} />
-        <Route path={routes.ResetPassword.path} element={<ResetPassword />} />
-        <Route path={routes.VerifyEmail.path} element={<VerifyEmail />} />
-        <Route path={routes.Dashboard.path}>
-          <Route index element={<RequireAuth element={<DashboardHome />} restrictedTo={restrictions.none} />} />
-          <Route
-            path={routes.Profile.path}
-            element={<RequireAuth element={<Profile />} restrictedTo={restrictions.none} />}
-          />
+    <div
+      className={`${transitionStage}`}
+      onAnimationEnd={() => {
+        if (transitionStage === 'fadeOut') {
+          setTransistionStage('fadeIn');
+          setDisplayLocation(location);
+        }
+      }}
+    >
+      <Routes>
+        <Route path={routes.Home.path} element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={routes.Login.path} element={<Login />} />
+          <Route path={routes.Register.path} element={<Register />} />
+          <Route path={routes.Lock.path} element={<Lock />} />
+          <Route path={routes.ResetPassword.path} element={<ResetPassword />} />
+          <Route path={routes.VerifyEmail.path} element={<VerifyEmail />} />
+          <Route path={routes.Dashboard.path}>
+            <Route index element={<RequireAuth element={<DashboardHome />} restrictedTo={restrictions.none} />} />
+            <Route
+              path={routes.Profile.path}
+              element={<RequireAuth element={<Profile />} restrictedTo={restrictions.none} />}
+            />
+          </Route>
         </Route>
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 };
 
