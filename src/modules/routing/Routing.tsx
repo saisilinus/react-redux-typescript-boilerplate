0ef5dd2c0e1afe88061a945159befe2979c74227
saisilinus <saisilinus19@gmail.com>
2022-05-07 11:32:25 +0300

    fix(routing.tsx): fix routing animation
    fix routing animation to support redirects

--- src/modules/routing/Routing.tsx
+++ src/modules/routing/Routing.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import RequireAuth from './RequireAuth';
 import { ForgotPassword, Login, NotFound, Register, ResetPassword, VerifyEmail } from '../auth';
 import { DashboardHome, Profile } from '../dashboard';
@@ -11,51 +12,31 @@ import SidebarLayout from '../dashboard/components/layout/SidebarLayout';
 
 const Routing = () => {
   const location = useLocation();
-  const [displayLocation, setDisplayLocation] = useState(location);
-  const [transitionStage, setTransistionStage] = useState('fadeIn');
-
-  useEffect(() => {
-    if (location !== displayLocation) setTransistionStage('fadeOut');
-  }, [location, displayLocation]);
-
   return (
-    <div
-      className={`${transitionStage}`}
-      onAnimationEnd={() => {
-        if (transitionStage === 'fadeOut') {
-          setTransistionStage('fadeIn');
-          setDisplayLocation(location);
-        }
-      }}
-    >
-      <Routes location={displayLocation}>
-        <Route path={routes.Home.path} element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path={routes.Login.path} element={<Login />} />
-          <Route path={routes.Register.path} element={<Register />} />
-          <Route path={routes.ResetPassword.path} element={<ResetPassword />} />
-          <Route path={routes.ForgotPassword.path} element={<ForgotPassword />} />
-          <Route path={routes.VerifyEmail.path} element={<VerifyEmail />} />
-          {/* <Route path={routes.Dashboard.path}>
-            <Route index element={<RequireAuth element={<DashboardHome />} restrictedTo={restrictions.none} />} />
-            <Route
-              path={routes.Profile.path}
-              element={<RequireAuth element={<Profile />} restrictedTo={restrictions.none} />}
-            />
-          </Route> */}
-        </Route>
-        <Route element={<SidebarLayout />}>
-          <Route path={`/${routes.Dashboard.path}`}>
-            <Route index element={<RequireAuth element={<DashboardHome />} restrictedTo={restrictions.none} />} />
-            <Route
-              path={routes.Profile.path}
-              element={<RequireAuth element={<Profile />} restrictedTo={restrictions.none} />}
-            />
+    <TransitionGroup component={null}>
+      <CSSTransition key={location.key} classNames="fade" timeout={500}>
+        <Routes>
+          <Route path={routes.Home.path} element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path={routes.Login.path} element={<Login />} />
+            <Route path={routes.Register.path} element={<Register />} />
+            <Route path={routes.ResetPassword.path} element={<ResetPassword />} />
+            <Route path={routes.ForgotPassword.path} element={<ForgotPassword />} />
+            <Route path={routes.VerifyEmail.path} element={<VerifyEmail />} />
           </Route>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+          <Route element={<SidebarLayout />}>
+            <Route path={`/${routes.Dashboard.path}`}>
+              <Route index element={<RequireAuth element={<DashboardHome />} restrictedTo={restrictions.none} />} />
+              <Route
+                path={routes.Profile.path}
+                element={<RequireAuth element={<Profile />} restrictedTo={restrictions.none} />}
+              />
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
