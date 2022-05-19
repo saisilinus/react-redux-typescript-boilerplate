d04b4d9dbb08e4fa814a031c53895eeadf000124
saisilinus <saisilinus19@gmail.com>
2022-05-19 10:56:53 +0300

    refactor(tsconfig.json): remove redundant entries
    remove redundant entries from include

--- src/modules/common/utils/index.ts
+++ src/modules/common/utils/index.ts
@@ -3,3 +3,5 @@ export { default as formatName } from './formatName';
 export { default as isEmpty } from './isEmpty';
 export { default as sanitize } from './sanitize';
 export { default as splitName } from './splitName';
+export { default as formattedYear } from './formattedYear';
+export { default as getPages } from './getPages';
